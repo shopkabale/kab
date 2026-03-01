1621e8af817f046dedaeed410109422e0a171103
kabaleonline <shopkabale@gmail.com>
2026-03-01 15:49:58 +0300

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com', // Required for Google Sign-In profile pictures
+      },
+    ],
+  },
+};
+
+export default nextConfig;
