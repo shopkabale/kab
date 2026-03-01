937a5801969b690fd52b77dbff53d15bf63734a1
kabaleonline <shopkabale@gmail.com>
2026-03-02 00:53:59 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,12 @@
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development", // Won't annoy you with caching while coding
+  register: true,
+  skipWaiting: true,
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -14,4 +23,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
