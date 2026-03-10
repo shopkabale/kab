7641d4a8a99c7a1cb44f287a21df98d6bb707313
kabaleonline <shopkabale@gmail.com>
2026-03-10 07:05:13 +0300

    Update WebsiteBanner.tsx

--- components/WebsiteBanner.tsx
+++ components/WebsiteBanner.tsx
@@ -13,7 +13,7 @@ const [index, setIndex] = useState(0);
 useEffect(() => {
 const interval = setInterval(() => {
 setIndex((prev) => (prev + 1) % messages.length);
-}, 3000); // 3 seconds
+}, 3000);
 
 return () => clearInterval(interval);
 
@@ -22,21 +22,21 @@ return () => clearInterval(interval);
 return (
 <div className="fixed top-0 left-0 w-full bg-black text-white z-[60] h-8 flex items-center">
 
-  {/* CENTERED ROTATING TEXT */}
-  <div className="absolute left-1/2 -translate-x-1/2 text-xs text-center">
+  {/* CENTERED TEXT (SAFE AREA) */}
+  <div className="absolute left-1/2 -translate-x-1/2 text-xs text-center px-4 max-w-[70%] truncate">
     <span key={index} className="animate-dropIn">
       {messages[index]}
     </span>
   </div>
 
-  {/* RIGHT SIDE BUTTONS */}
-  <div className="ml-auto flex items-center gap-3 pr-4">
+  {/* RIGHT SIDE */}
+  <div className="ml-auto flex items-center gap-3 pr-4 min-w-[120px] justify-end">
     <a
       href="https://wa.me/256759997376"
       target="_blank"
       className="bg-white text-black px-3 py-[2px] rounded text-xs font-semibold"
     >
-      Contact Us
+      Contact
     </a>
 
     <button
