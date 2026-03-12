e06f8ea7d55737bb8da54409f7b2151a0c104fc2
kabaleonline <shopkabale@gmail.com>
2026-03-12 13:46:08 +0300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -3,13 +3,12 @@ import { NextResponse } from 'next/server';
 export function middleware(request) {
   // 1. Check if the user is trying to access an /admin route
   if (request.nextUrl.pathname.startsWith('/admin')) {
-    
+
     // 2. Check for our custom admin cookie
     const hasAdminCookie = request.cookies.get('kabale_admin_session')?.value === 'true';
 
     // 3. If the cookie is missing, instantly bounce them to the home page
     if (!hasAdminCookie) {
-      // You can redirect them to '/' or a custom '/unauthorized' page
       return NextResponse.redirect(new URL('/', request.url));
     }
   }
