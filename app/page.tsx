d0a930c136b11f619e97e7b85374d0d573bd293a
kabaleonline <shopkabale@gmail.com>
2026-04-05 08:34:43 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -117,7 +117,7 @@ export default async function Home() {
           <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
           </svg>
-          Tap to call 075 999 7376 to report a scam or undelivery
+          Tap to call (0759997376) to report a scam or undelivery
         </a>
       </section>
 
